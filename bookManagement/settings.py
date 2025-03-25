d1800d33936b547e349b76890128e4eeebc2aeaf
alimurtaza <alideveromurtaza110@gmail.com>
2025-03-25 13:19:15 +0500

    set secret key to .env

--- bookManagement/settings.py
+++ bookManagement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 from dotenv import load_dotenv
 load_dotenv()
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
